4d6622cd284e2e63ae8dacf81949375e3c5e52f9
BhavyaHirani <109067012+BhavyaHirani@users.noreply.github.com>
2023-04-03 16:15:29 +0200

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,81 @@
+import cv2
+
+img = cv2.imread("solar-system.jpg")
+
+cv2.putText(
+    img,
+    "Sun",
+    (20,300),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Mercury",
+    (125,185),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.3,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Venus",
+    (190,175),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Earth",
+    (285,170),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Mars",
+    (385,180),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Jupiter",
+    (500,100),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Saturn",
+    (800,150),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Uranus",
+    (960,140),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+cv2.putText(
+    img,
+    "Neptune",
+    (1100,150),
+    cv2.FONT_HERSHEY_COMPLEX,
+    0.5,
+    (0,255,0)
+)
+
+cv2.imwrite("Solar-systemwWithNNames.jpg", img)
+
+cv2.imshow("Output",img)
+cv2.waitKey(0)
